dff2d939819e1b3c8adcb4dcd9ac88c1c2755e04
mateppon <margit.tepponen@helsinki.fi>
2020-11-10 21:05:06 +0200

    update Unicafe-tests

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,25 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
-}
+    @Test
+    public void saldoAlussaOikein(){
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    @Test 
+    public void rahanLataaminenKasvattaaSaldoaOikein(){
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+    @Test
+    public void rahanOttaminenToimiiKunOnRiittavastiRahaa(){
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+    @Test
+    public void rahanOttaminenToimiiKunEiOleriittavastiRahaa(){
+        kortti.otaRahaa(20);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    }
+    
